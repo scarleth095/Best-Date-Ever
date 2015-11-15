c21d3861396c2e3849d18a893b7b6df724ffcd42
Scarleth Estevez <scarleth@bu.edu>
2015-11-15 05:18:18 -0500

    a

--- static/animate.js
+++ static/animate.js
@@ -1,22 +1,17 @@
 function fadeHeart(){
 	$("#restaurants").hide();
-	$("#plan").hide();
 	$("#movies").hide();
 	$("#other").hide();
 	$("#pad").hide();
-	$("#q1").hide();
-	$("#q2").hide();
-	$("#q3").hide();
-	$("#data").hide();
+	$(".q").hide();
+	
 
 	$("#start").click(function(){
 	    $("#fade1").fadeOut(2900);
 	    $("#start").fadeOut(2900,function(){
 	    	$("#pad").show();
-	    	$("#q1").fadeIn();
-	    	$("#q2").fadeIn();
-			$("#q3").fadeIn();
-			$("#plan").fadeIn();
+	    	$(".q").fadeIn();
+	    	
 
     	});
 	});
