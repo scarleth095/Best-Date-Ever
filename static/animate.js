cac373bba2615690aedd8e2bb57ad76742540f00
Scarleth Estevez <scarleth@bu.edu>
2015-11-15 03:19:45 -0500

    new web page

--- static/animate.js
+++ static/animate.js
@@ -1,4 +1,23 @@
+$(document).ready(function(){
 
+	$("#q1").hide();
+	$("#q2").hide();
+	$("#q3").hide();
+
+	$("#start").click(function(){
+    $("#fade1").fadeOut(2900);
+    $("#start").fadeOut(2900,function(){
+    	$("#q1").fadeIn();
+
+    });
+    
+  
+
+
+	});
+
+
+});
 
 
 
